35b0d236f4e7c13b2fe993078c9e2ff71690e913
Fernando Gómez <gomezhyuuga@gmail.com>
2015-09-13 22:14:10 -0500

    Versión inicial del prototipo

--- DownUp/AboutViewController.swift
+++ DownUp/AboutViewController.swift
@@ -1,25 +1,26 @@
 //
-//  SecondViewController.swift
+//  AboutViewController.swift
 //  DownUp
 //
 //  Created by Fernando Gómez Herrera on 9/13/15.
 //  Copyright (c) 2015 Fernando Gómez. All rights reserved.
 //
 
+import Foundation
+
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class AboutViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
---
+++ DownUp/StatisticsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  DownUp
+//
+//  Created by Fernando Gómez Herrera on 9/13/15.
+//  Copyright (c) 2015 Fernando Gómez. All rights reserved.
+//
+
+import UIKit
+
+class StatisticsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
