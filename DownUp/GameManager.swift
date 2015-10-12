3318e838b8be7619bfc12fe58b4d986ccf8a8d18
aileennag <aileen-hola@hotmail.com>
2015-10-12 17:31:08 -0500

    Update GameManager.swift

--- DownUp/GameManager.swift
+++ DownUp/GameManager.swift
@@ -9,10 +9,20 @@
 import Foundation
 
 class GameManager {
-    var items: [Item] = [
-        Item(img: "osito.jpg", price: 20),
-        Item(img: "asd.jpg", price: 100)
+    var objects: [Item] = [
+        Item(img: "osito.jpg", price: 2),
+        Item(img: "asd.jpg", price: 8),
+        Item(img: “cho.jpg", price: 7),
+        Item(img: “pan.jpg", price: 1),
+        Item(img: “cereal.jpg", price: 4),
+        Item(img: “leche.jpg", price: 5),
+        Item(img: “libro.jpg", price: 3),
+        Item(img: “ipad.jpg", price: 9),
+        Item(img: “lapiz.jpg", price: 6),
+        Item(img: “dulce.jpg", price: 4),
+       
     ]
+
     var itemToBuy: Item?
     var moneyAcum: Int
     
