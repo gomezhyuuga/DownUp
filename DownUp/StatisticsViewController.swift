665fcafdca6f78c9a0af411e4bd491eeeb4c200c
BlueHorizon <kingboo186@gmail.com>
2015-11-26 15:14:44 -0600

    Test Chart
    Code is right but stupid Xcode insists on exploding.

--- DownUp/StatisticsViewController.swift
+++ DownUp/StatisticsViewController.swift
@@ -11,16 +11,40 @@ import Charts
 
 class StatisticsViewController: UIViewController {
 
-    @IBOutlet weak var barChartView: BarChartView!
+    @IBOutlet weak var graphView: BarChartView!
+    
+    var daysOfTheWeek: [String] = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes"];
+    var times: [Double] = [];
     
     override func viewDidLoad() {
         super.viewDidLoad();
         // Do any additional setup after loading the view, typically from a nib.
-        setupChart();
+        times = readData(2);
+        setupChart(daysOfTheWeek, values: times);
     }
     
-    private func setupChart() {
-        barChartView.noDataText = "You have to load historical data.";
+    private func setupChart(weekDays: [String], values: [Double]) {
+        graphView.noDataText = "No hay datos históricos.";
+        graphView.noDataTextDescription = "Contacte a su administrador.";
+        
+        var dataEntries: [BarChartDataEntry] = [];
+        
+        for i in 0 ..< daysOfTheWeek.count {
+            let entry = BarChartDataEntry(value: values[i], xIndex: i);
+            dataEntries.append(entry);
+        }
+        
+        let dataSet = BarChartDataSet(yVals: dataEntries, label: "Tiempos por Día");
+        dataSet.colors = ChartColorTemplates.colorful();
+        
+        let graphData = BarChartData(xVals: weekDays, dataSet: dataSet);
+        graphView.data = graphData;
+        //graphView.descriptionText = "Minutos";
+    }
+    
+    private func readData(level: Int) -> [Double] {
+        let levelTimes = [1.0, 2.0, 3.0, 4.0, 5.0];
+        return levelTimes;
     }
 
     override func didReceiveMemoryWarning() {
