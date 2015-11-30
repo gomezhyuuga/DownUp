49b97c087d2c80f92635fa790a05a066f8c68a8e
Fernando Gómez <gomezhyuuga@gmail.com>
2015-11-30 00:47:11 -0600

    Hide topbar

--- DownUp/MainMenuViewController.swift
+++ DownUp/MainMenuViewController.swift
@@ -12,30 +12,11 @@ class MainMenuViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-        
     }
-    
-    
-    @IBAction func exitButton(sender: AnyObject) {
-        exit(0)
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
